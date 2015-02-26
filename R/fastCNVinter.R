e4faa7c6db496082adb584456469037d85924137
Isaac Subirana <isubirana@imim.es>
2015-02-26 00:00:00 +0000

    version 2.1

--- R/fastCNVinter.R
+++ R/fastCNVinter.R
@@ -1,4 +1,4 @@
-fastCNVassoc <- function(probs, formula, data, model = "additive",
+fastCNVinter <- function(probs, formula, data, model = "additive",
     family = "binomial", nclass = 3, colskip = 5, tol = 1e-06, max.iter = 30, verbose = FALSE, multicores=0)
 {
     cl <- match.call()
@@ -53,10 +53,10 @@ fastCNVassoc <- function(probs, formula, data, model = "additive",
         fit0<-survreg(Surv(Y,CENS)~1)
     }
     if (family!="weibull")
-      PARAM<-c(0,coef(fit0))
+      PARAM<-c(0,0,0,coef(fit0))
     else
-      PARAM<-c(0,unlist(translate(fit0)))
-    PARAM[1:2]<-PARAM[2:1] 
+      PARAM<-c(0,0,0,unlist(translate(fit0)))
+    PARAM[c(1,4)]<-PARAM[c(4,1)] 
     
     # OTHER INPUTS
     TOL <- tol
@@ -83,69 +83,89 @@ fastCNVassoc <- function(probs, formula, data, model = "additive",
         stop(" 'probs' must be a character (file) or a matrix or a data.frame")        
       nCNVs<-nrow(PROBS)
     }
+    mm<-outer(1:nCNVs,1:nCNVs,paste,sep=",")
+    mm<-mm[upper.tri(mm)]
     
     # RUN
     if (multicores==0){  # no use of parallel package
-      ans<-lapply(1:nCNVs, function(i){
+      ans<-lapply(mm, function(index){
+        i<-as.integer(strsplit(index,split=",")[[1]][1])
+        j<-as.integer(strsplit(index,split=",")[[1]][2])
         if (verbose)
-          cat("Iteration ",i,"\n")      
+          cat("Pair ",index,"\n")      
         if (is.character(probs)){
           PROBS.i<-strsplit(PROBS[i],split=" ")[[1]]
-        }else
+          PROBS.j<-strsplit(PROBS[j],split=" ")[[1]]
+        }else{
           PROBS.i<-PROBS[i,]
-        if (colskip>0)
+          PROBS.j<-PROBS[j,]
+        }
+        if (colskip>0){
           PROBS.i<-PROBS.i[-(1:colskip)]
-        WW<-matrix(as.double(PROBS.i),nrow=nclass)
+          PROBS.j<-PROBS.j[-(1:colskip)]
+        }
+        WW.i<-matrix(as.double(PROBS.i),nrow=nclass)
+        WW.j<-matrix(as.double(PROBS.j),nrow=nclass)
+        WW<- .Call("outerC",as.integer(N),t(WW.i),t(WW.j))
         if (nCov > 0){
           if (family=="binomial")
-            fit <- try(.Call("NRlogisticcov", N, PARAM, P, Y, WW, Q, C, TOL, MAX_ITER, VERBOSE))
+            fit <- try(.Call("NRinterlogisticcov", N, PARAM, Y, WW, Q, C, TOL, MAX_ITER, VERBOSE))
           if (family=="weibull")
-            fit <- try(.Call("NRweibullcov", N, PARAM, P, Y, CENS, WW, Q, C, TOL, MAX_ITER, VERBOSE))
+            fit <- try(.Call("NRinterweibullcov", N, PARAM, Y, CENS, WW, Q, C, TOL, MAX_ITER, VERBOSE))
         } else{ 
           if (family=="binomial")
-            fit <- try(.Call("NRlogistic", N, PARAM, P, Y, WW, TOL, MAX_ITER, VERBOSE))
+            fit <- try(.Call("NRinterlogistic", N, PARAM, Y, WW, TOL, MAX_ITER, VERBOSE))
           if (family=="weibull")
-            fit <- try(.Call("NRweibull", N, PARAM, P, Y, CENS, WW, TOL, MAX_ITER, VERBOSE))          
+            fit <- try(.Call("NRinterweibull", N, PARAM, Y, CENS, WW, TOL, MAX_ITER, VERBOSE))          
         }
         if (inherits(fit,"try-error")) 
           return(rep(NA,3))
-        logor<-fit[[1]][2]
-        se<-try(sqrt(diag(solve(-fit[[4]])))[2],silent=TRUE)
+        logor<-fit[[1]][4]
+        se<-try(sqrt(diag(solve(-fit[[4]])))[4],silent=TRUE)
         if (inherits(se,"try-error")) 
-          se<-NA
+          se<-NA            
         iter <- fit[[5]]
         return(c(logor,se,iter))
       })
       ans<-matrix(unlist(ans),ncol=3,byrow=TRUE)
     } else {            # use of parallel package
       requireNamespace("parallel", quietly=TRUE)
-      ans<-parallel::mclapply(1:nCNVs, function(i){
+      ans<-parallel::mclapply(mm, function(index){
+        i<-as.integer(strsplit(index,split=",")[[1]][1])
+        j<-as.integer(strsplit(index,split=",")[[1]][2])
         if (verbose)
-          cat("Iteration",i,"\n")      
+          cat("Pair ",index,"\n")    
         if (is.character(probs)){
           PROBS.i<-strsplit(PROBS[i],split=" ")[[1]]
-        } else
+          PROBS.j<-strsplit(PROBS[j],split=" ")[[1]]
+        } else {
           PROBS.i<-PROBS[i,]
-        if (colskip>0)
+          PROBS.j<-PROBS[j,]
+        }
+        if (colskip>0){
           PROBS.i<-PROBS.i[-(1:colskip)]
-        WW<-matrix(as.double(PROBS.i),nrow=nclass)
+          PROBS.j<-PROBS.j[-(1:colskip)]
+        }
+        WW.i<-matrix(as.double(PROBS.i),nrow=nclass)
+        WW.j<-matrix(as.double(PROBS.j),nrow=nclass)
+        WW<- .Call("outerC",as.integer(N),t(WW.i),t(WW.j))
         if (nCov > 0){
           if (family=="binomial")
-            fit <- try(.Call("NRlogisticcov", N, PARAM, P, Y, WW, Q, C, TOL, MAX_ITER, VERBOSE))
+            fit <- try(.Call("NRinterlogisticcov", N, PARAM, Y, WW, Q, C, TOL, MAX_ITER, VERBOSE))
           if (family=="weibull")
-            fit <- try(.Call("NRweibullcov", N, PARAM, P, Y, CENS, WW, Q, C, TOL, MAX_ITER, VERBOSE))
+            fit <- try(.Call("NRinterweibullcov", N, PARAM, Y, CENS, WW, Q, C, TOL, MAX_ITER, VERBOSE))
         }else{ 
           if (family=="binomial")
-            fit <- try(.Call("NRlogistic", N, PARAM, P, Y, WW, TOL, MAX_ITER, VERBOSE))
+            fit <- try(.Call("NRinterlogistic", N, PARAM, Y, WW, TOL, MAX_ITER, VERBOSE))
           if (family=="weibull")
-            fit <- try(.Call("NRweibull", N, PARAM, P, Y, CENS, WW, TOL, MAX_ITER, VERBOSE))          
+            fit <- try(.Call("NRinterweibull", N, PARAM, Y, CENS, WW, TOL, MAX_ITER, VERBOSE))          
         }
         if (inherits(fit,"try-error")) 
           return(rep(NA,3))
-        logor<-fit[[1]][2]
-        se<-try(sqrt(diag(solve(-fit[[4]])))[2],silent=TRUE)
+        logor<-fit[[1]][4]
+        se<-try(sqrt(diag(solve(-fit[[4]])))[4],silent=TRUE)
         if (inherits(se,"try-error")) 
-          se<-NA        
+          se<-NA    
         iter <- fit[[5]]
         return(c(logor,se,iter))
       },mc.cores=multicores)
@@ -159,7 +179,7 @@ fastCNVassoc <- function(probs, formula, data, model = "additive",
    else
      ans <- cbind(ans[,1:2], zscore, pval, ans[,3])
    colnames(ans)<-c("beta","se","zscore","pvalue","iter")
-   ans<-data.frame("variant"=1:nrow(ans),ans)
+   ans<-data.frame("pair"=mm,ans)
    ans
 
 }
--- R/getProbs.cghCall.r
+++ R/getProbs.cghCall.r
@@ -3,16 +3,18 @@ function (x)
 {
     if (!inherits(x, "cghCall"))
         stop("object must be of class 'cghCall'")
-    Clone <- featureNames(x)
-    Chromo <- chromosomes(x)
-    BPstart <- bpstart(x)
-    BPend <- bpend(x)
-    Calls <- calls(x)
-    Probloss <- probloss(x)
-    Probnorm <- probnorm(x)
-    Probgain <- probgain(x)
+    requireNamespace("Biobase", quietly = TRUE)
+    requireNamespace("CGHbase", quietly = TRUE)
+    Clone <- Biobase::featureNames(x)
+    Chromo <- CGHbase::chromosomes(x)
+    BPstart <- CGHbase::bpstart(x)
+    BPend <- CGHbase::bpend(x)
+    Calls <- CGHbase::calls(x)
+    Probloss <- CGHbase::probloss(x)
+    Probnorm <- CGHbase::probnorm(x)
+    Probgain <- CGHbase::probgain(x)
     colnam <- c(colnames(Probloss), colnames(Probnorm), colnames(Probgain))
-    if (is.null(probamp(x))) {
+    if (is.null(CGHbase::probamp(x))) {
         allprob <- c()
         ncl <- ncol(Probnorm)
         for (i in 1:ncl) {
@@ -24,7 +26,7 @@ function (x)
         }
     }
     else {
-        Probamp <- probamp(x)
+        Probamp <- CGHbase::probamp(x)
         colnam <- c(colnam, colnames(Probamp))
         allprob <- c()
         ncl <- ncol(Probnorm)
